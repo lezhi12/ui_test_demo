e39c202e939e46d76f340e452851118e5664a9b6
lezhi12 <lezhiworld12@gmail.com>
2025-07-12 07:04:31 +0800

    增加空activity，exported为true，用于测试是否可以直接调起

---
+++ app/src/main/java/com/example/myapplication/EmptyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmptyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 空Activity，未设置布局
+    }
+}
