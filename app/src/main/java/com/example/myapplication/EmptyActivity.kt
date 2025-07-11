1e18f8179168a1776c99bde881dea16a8f021f74
lezhi12 <lezhiworld12@gmail.com>
2025-07-12 07:35:35 +0800

    1.emptyactivity中增加一行文案，以醒目显示是该activity。2.修复了通过adb命令调起emptyactivity时候一闪而过的问题，问题原因是EmptyActivity继承自AppCompatActivity，需要使用AppCompat主题，而原主题基于Material主题导致冲突。debug过程:logcat中有闪退堆栈，给到Trae，定位到了该问题。

--- app/src/main/java/com/example/myapplication/EmptyActivity.kt
+++ app/src/main/java/com/example/myapplication/EmptyActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.view.Gravity
 
 class EmptyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 空Activity，未设置布局
+        val textView = TextView(this)
+        textView.text = "我可以被直接调起"
+        textView.textSize = 24f
+        textView.gravity = Gravity.CENTER
+        setContentView(textView)
     }
 }
